d9adb9419c309812197acf47d8302def9a412aa2
Harkirat Singh Panesar <panesarh@uwindsor.ca>
2022-02-18 19:13:11 -0500

    Code for login page

--- App.js
+++ App.js
@@ -1,31 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import HomeScreen from './screens/HomeScreen';
+// import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import {Font} from 'expo';
+import { StyleSheet, Text, View, Button, SafeAreaView, ActivityIndicator, TextInput,TouchableOpacity } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+// type Props = {};
+// export default class App extends Component<Props> {
+export default class App extends Component {
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} /> 
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  
+  render() {
+    return (
+      <SafeAreaView style = {styles.container}>
+        <Text style = {styles.gamename}>Hallway Lookers</Text>
+        <TextInput style = {styles.input} placeholder = "Username"/>
+        <TextInput style = {styles.password} placeholder = "Password" secureTextEntry/>
+        <View style = {styles.btnContainer}>
+          <TouchableOpacity style = {styles.userButton} onPress = {() => alert("Login Works")}>
+            <Text style = {styles.btnText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style = {styles.userButton} onPress = {() => alert("Signup Works")}>
+            <Text style = {styles.btnText}>Signup</Text>
+          </TouchableOpacity>
+        </View>
+        <View style = {styles.frgtBtnContainer}>
+        <TouchableOpacity style = {styles.forgotPassBtn} onPress = {() => alert("This Works")}>
+            <Text style = {styles.forgotPassText}>Forgot password?</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFC955',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gamename: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff',
+  },
+  input: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginBottom: 10
+  },
+  password: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginBottom: 10
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%"
+  },
+  userButton: {
+    backgroundColor: "#47006D",
+    padding: 15,
+    width: "45%"
+  },
+  btnText: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  forgotPassBtn: {
+    padding: 15
+  }
 });
