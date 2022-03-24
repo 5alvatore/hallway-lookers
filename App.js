23774ed2ba9a707c20b6ad7a3999b3e307bbba37
Harkirat Singh Panesar <panesarh@uwindsor.ca>
2022-03-24 10:33:13 -0400

    Username, Homepage, AR Music changes
    Username, Homepage and AR Music implementation

--- App.js
+++ App.js
@@ -1,66 +1,145 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ResetPasswordScreen from './screens/ResetPasswordScreen';
-import HomeScreen from './screens/HomeScreen';
-import ViroReactTestScreen from './screens/ViroReactTestScreen';
-import MiniGameOneScreen from './screens/MiniGameOneScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ResetPasswordScreen from "./screens/ResetPasswordScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ViroReactTestScreen from "./screens/ViroReactTestScreen";
+import MiniGameOneScreen from "./screens/MiniGameOneScreen";
+import { auth, database } from "./firebase";
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerItem,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 
-import ProfileScreen from './screens/ProfileScreen';
-import PathwayScreen from './screens/PathwayScreen';
-import RankingScreen from './screens/RankingScreen';
-import SignOutScreen from './screens/SignOutScreen';
+import ProfileScreen from "./screens/ProfileScreen";
+import PathwayScreen from "./screens/PathwayScreen";
+import RankingScreen from "./screens/RankingScreen";
+// import SignOutScreen from "./screens/SignOutScreen";
+
+var user = null;
+var name = [];
+
+user = auth.currentUser;
+// console.log("User:: " + database.ref("/users" + user.uid + "/name"));
+
+var title;
+const displayInfo = database
+  .ref()
+  .child("users")
+  .child(user.uid)
+  // .child("name")
+  .get()
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      name = [];
+      if (snapshot.val().name != undefined && snapshot.val().name.length > 0) {
+        let title_string_array = snapshot.val().name.split(" ");
+        // title_string_array.pop();
+        title = title_string_array[0];
+        // for (let i = 0; i < snapshot.val().name.length; i++) {
+        //   let title_string_array = snapshot.val().name[i];
+        //   title += title_string_array;
+        // }
+        console.log(title);
+      } else {
+        name = [];
+      }
+      // snapshot.val();
+    } else {
+      console.log("No data available");
+    }
+  });
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const Drawer = createDrawerNavigator()
-
-function DrawerRoutes() {
+const DrawerRoutes = () => {
   return (
-    <Drawer.Navigator screenOptions={{
-      drawerStyle: {
-        backgroundColor: '#363636',
-        width: 200,
-      },
-      drawerActiveTintColor: "#ffffff",
-      drawerInactiveTintColor: "#84c9fb",
-      headerStyle: { backgroundColor: "#121212" },
-      headerTintColor: "#dee2e6"
-    }}>
-      <Drawer.Screen name="Home" component={HomeScreen}
-      />
+    <Drawer.Navigator
+      // initialRouteName="Drawer"
+      // drawerContent={(props) => <DrawerContent {...props} />}
+      drawerContent={(props) => {
+        return (
+          <DrawerContentScrollView {...props}>
+            <View style={styles.userInfoSection}>
+              <View style={{ flexDirection: "row", marginTop: 15 }}>
+                <Text style={styles.username}>Username</Text>
+                <Text style={styles.title}>{title}</Text>
+                {/* <Text style={styles.title}>Harkirat</Text> */}
+              </View>
+            </View>
+            <DrawerItemList {...props} />
+            <DrawerItem
+              labelStyle={{ color: "#84c9fb" }}
+              label="Signout"
+              // onPress={() => props.navigation.navigate("Login")}
+              onPress={() =>
+                auth
+                  .signOut()
+                  .then(() => {
+                    props.navigation.navigate("Login");
+                  })
+                  .catch((error) => alert(error.message))
+              }
+            />
+          </DrawerContentScrollView>
+        );
+      }}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "#363636",
+          width: 200,
+        },
+        drawerActiveTintColor: "#ffffff",
+        drawerInactiveTintColor: "#84c9fb",
+        headerStyle: { backgroundColor: "#121212" },
+        headerTintColor: "#dee2e6",
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
       <Drawer.Screen name="Pathways" component={PathwayScreen} />
       <Drawer.Screen name="Rankings" component={RankingScreen} />
-      <Drawer.Screen name="Signout" component={SignOutScreen} />
+      {/* <Drawer.Screen name="Signout" component={SignOutScreen} /> */}
     </Drawer.Navigator>
   );
-}
+};
 
 const navTheme = {
   colors: {
-    background: '#121212',
+    background: "#121212",
   },
 };
 
 export default function App() {
   return (
     <NavigationContainer theme={navTheme}>
-      <Stack.Navigator initialRouteName="Hallway Lookers"
+      <Stack.Navigator
+        initialRouteName="Hallway Lookers"
         screenOptions={{
           headerStyle: { backgroundColor: "#121212" },
-          headerTintColor: "#dee2e6"
-        }}>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          headerTintColor: "#dee2e6",
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Hallway Lookers" component={DrawerRoutes} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Hallway Lookers"
+          component={DrawerRoutes}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ViroReactTest" component={ViroReactTestScreen} />
         <Stack.Screen name="MiniGameOne" component={MiniGameOneScreen} />
@@ -72,11 +151,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   drawerStyles: {
-    backgroundColor: 'black',
-  }
+    backgroundColor: "black",
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    marginLeft: 10,
+    fontWeight: "bold",
+    color: "#84c9fb",
+  },
+  username: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: "bold",
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
 });
--- screens/ViroReactTestScreen.js
+++ screens/ViroReactTestScreen.js
@@ -1,5 +1,14 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Button, StyleSheet, Icon, TextInput } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+  Icon,
+  TextInput,
+} from "react-native";
+import { Audio } from "expo-av";
 import {
   ViroARScene,
   ViroText,
@@ -12,9 +21,8 @@ import {
   ViroAmbientLight,
   ViroARTrackingTargets,
   ViroARImageMarker,
-  ViroFlexView
-} from '@viro-community/react-viro';
-
+  ViroFlexView,
+} from "@viro-community/react-viro";
 
 const InitialScene = (props) => {
   let data = props.sceneNavigator.viroAppProps;
@@ -26,61 +34,105 @@ const InitialScene = (props) => {
         // rotateX: "+=360",
         rotateY: "+=360",
         // rotateZ: "+=360"
-      }
-    }
-  })
+      },
+    },
+  });
 
   ViroARTrackingTargets.createTargets({
     dummyImage: {
-      source: require('../assets/juice.jpeg'),
-      orientation: 'Up',
-      physicalWidth: 0.165
-    }
-  })
+      source: require("../assets/juice.jpeg"),
+      orientation: "Up",
+      physicalWidth: 0.165,
+    },
+  });
 
   // Adding log once image is detected
   const anchorFound = () => {
-    console.log("Anchor / Image detected")
+    console.log("Anchor / Image detected");
+    // playSound();
+    soundOn();
+  };
+
+  // const [sound, setSound] = React.useState();
+  const [audioStatus, setAudioStatus] = useState(false);
+  const [sound, setSound] = React.useState(new Audio.Sound());
+
+  async function soundOn() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/ARMusic/SneakyAdventure.mp3")
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
   }
+
+  async function soundOff() {
+    console.log("Sound off");
+    // await sound.stopAndUnloadAsync();
+    await sound.stopAsync();
+    await sound.unloadAsync();
+  }
+
+  // React.useEffect(() => {
+  //   return sound
+  //     ? () => {
+  //         console.log("Unloading Sound");
+  //         sound.stopAsync();
+  //         sound.unloadAsync();
+  //       }
+  //     : undefined;
+  // }, [audioStatus]);
 
   return (
     <ViroARScene>
       <ViroFlexView
-          height={1}
-          width={4.5}
-          position={[0, 2, -10]}
-          rotation={[360,0,0]}>
-          <ViroFlexView backgroundColor={'blue'} style={{ flex: 0.8, flexDirection: 'row' }} >
-            <ViroText
-              style={{ color: 'white', flex: 1 }}
-              text={'Welcome to AR !!'}
-              fontSize={50} />
-          </ViroFlexView>
+        height={1}
+        width={4.5}
+        position={[0, 2, -10]}
+        rotation={[360, 0, 0]}
+      >
+        <ViroFlexView
+          backgroundColor={"blue"}
+          style={{ flex: 0.8, flexDirection: "row" }}
+        >
+          <ViroText
+            style={{ color: "white", flex: 1 }}
+            text={"Welcome to AR !!"}
+            fontSize={50}
+          />
         </ViroFlexView>
+      </ViroFlexView>
       <ViroARImageMarker target="dummyImage" onAnchorFound={anchorFound}>
-      <ViroAmbientLight color="#ffffff" />
+        <ViroAmbientLight color="#ffffff" />
         <ViroAmbientLight color="#ffffff" />
         <Viro3DObject
-          source={require('../assets/easter-egg/12172_Egg_v1_l2.obj')}
+          source={require("../assets/easter-egg/12172_Egg_v1_l2.obj")}
           position={[0, -3, 0]}
           scale={[0.008, 0.008, 0.008]}
           rotation={[180, 0, 0]}
-          type="OBJ" />
+          type="OBJ"
+          onClick={() => {
+            // setAudioStatus(!audioStatus);
+            soundOff();
+          }}
+        />
       </ViroARImageMarker>
     </ViroARScene>
-  )
-}
+  );
+};
 
 export default () => {
-  const [obj, setObject] = useState('egg')
+  const [obj, setObject] = useState("egg");
 
   return (
     <View style={styles.mainView}>
       <ViroARSceneNavigator
         initialScene={{
-          scene: InitialScene
+          scene: InitialScene,
         }}
-        viroAppProps={{ "obj": Object }}
+        viroAppProps={{ obj: Object }}
         style={{ flex: 1 }}
       />
     </View>
@@ -90,25 +142,25 @@ export default () => {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#1e90ff'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1e90ff",
   },
   mainView: {
-    flex: 1
+    flex: 1,
   },
   controlsView: {
-    width: '100%',
+    width: "100%",
     height: 100,
-    backgroundColor: '#ffffff',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    backgroundColor: "#ffffff",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   text: {
     margin: 15,
-    backgroundColor: '#9d9d9d',
+    backgroundColor: "#9d9d9d",
     padding: 10,
-    fontWeight: 'bold'
-  }
+    fontWeight: "bold",
+  },
 });
